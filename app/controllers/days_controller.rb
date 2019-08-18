a52e575ebc26f3e92499bd9c8b6d10e13640e1b0
musicprogram <juan-o-henao@hotmail.es>
2019-08-18 10:46:42 -0500

    agregando metodos (create , destroy) al controlador de days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,9 +1,12 @@
 class DaysController < ApplicationController
+	before_action :set_day, only: [:destroy, :new, :create]
 
 	def create
     @day = Day.create(day_params)
+    @day.movie_id = @movie.id #asociando el id de la pelicula
     render json: @day
   end
+
 	def destroy #destruir 
     @day = Day.find(params[:id])
     if @day.destroy
@@ -15,7 +18,13 @@ class DaysController < ApplicationController
 
   private
 
+
+  def set_day
+    @movie = Movie.find(params[:movie_id]) # recupera el proyecto
+    @day = Day.find(params[:id]) if params[:id] # recupera el id 
+	end
+
   def day_params
-    params.require(:day).permit(:day_name)
+    params.require(:day).permit(:day_name, :movie_id)
   end
 end

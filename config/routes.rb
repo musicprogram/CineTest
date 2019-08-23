7c59f4a57c171ebaf6c399ec2d380fbc41e7f032
musicprogram <juan-o-henao@hotmail.es>
2019-08-23 11:08:41 -0500

    rerservations controller, routes, models

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	resources :movies
-	resources :days  #para que una pelicula pueda ser asignado a muchos días
+	resources :days  
+	resources :reservations
 
-	resources :movies do
-		resources :reservations
-	end
-
-	get 'movies/search'
 
 end
